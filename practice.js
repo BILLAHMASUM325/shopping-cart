abb637983610563c769605e6ff05a3a8c311c395
BILLAHMASUM325 <masumbillah3.25@outlook.com>
2021-12-24 22:07:39 +0600

    shopping-cart

---
+++ practice.js
@@ -0,0 +1,31 @@
+// let x = 10;
+// let y = 10;
+
+// let z = x + y;
+
+// console.log(z);
+
+// let x = 10
+// let y = 10;
+// let z = x * y;
+// console.log(z);
+
+// let x = 12;
+// let y = 2;
+
+// let z = x - y;
+// console.log(z);
+
+// let x = 20;
+// let y = 2;
+// let z = x / y;
+// console.log(z);
+
+// let x = 9;
+// let y = 2;
+// let z = x % y;
+// console.log(z);
+
+
+
+
